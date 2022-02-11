ec239f7afd2d1245982edb8a28b3f2ec15d37658
leticiadamasio <damasioleticia03@gmail.com>
2022-02-11 01:07:19 -0300

    Usando o map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Crie um array de objetos no STATE com pelo menos 6 objetos
+class App extends Component {
+state = {
+  persons: [
+    {
+      Nome: "Letícia",
+      Idade: 18,
+      Cidade: "São Gonçalo",
+      Profissão: "Desempregada no momento"
+
+  },
+    {
+      Nome: "Elaine",
+      Idade: 42,
+      Cidade: "São Gonçalo",
+      Profissão: "Professora"
+
+  },
+    {
+      Nome: "Lara",
+      Idade: 10,
+      Cidade: "São Gonçalo",
+      Profissão: "Estudante"
+    
+  },
+    {
+      Nome: "Walcir",
+      Idade: 44,
+      Cidade: "São Gonçalo",
+      Profissão: "Metalúrgico"
+
+  },
+    {
+      Nome: "Artur",
+      Idade: 18,
+      Cidade: "Rio de Janeiro",
+      Profissão: "Arquiteto"
+
+  },
+    {
+      Nome: "Dan",
+      Idade: 23,
+      Cidade: "Rio de Janeiro",
+      Profissão: "Professor e Developer"
+
+  },
+    {
+      Nome: "Fernando",
+      Idade: 24,
+      Cidade: "Rio de Janeiro",
+      Profissão: "Facilitador e Developer/Semideus"
+
+  },
+    {
+      Nome: "Ana Paula",
+      Idade: 43,
+      Cidade: "São Gonçalo",
+      Profissão: "Doméstica"
+
+  },
+    {
+      Nome: "Ana Júlia",
+      Idade: 12,
+      Cidade: "São Gonçalo",
+      Profissão: "Estudante"
+
+  }
+]
 }
-
-export default App;
+render(){
+return (
+  <main>
+    {this.state.persons.map((item) => 
+(
+      <div className="container">
+       <h2>Nome: {item.Nome}</h2>
+       <p>Idade: {item.Idade}</p>
+       <p>Cidade: {item.Cidade}</p>
+       <p>Profissão: {item.Profissão}</p>
+      </div>
+    ))}
+  </main>
+)
+}
+}
+export default App
